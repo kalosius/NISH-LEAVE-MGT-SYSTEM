c023bcdbaf50a10f13f00256b89a4f4be7818764
MUGISHA-ANDREW <andrewmugisha699@gmail.com>
2025-12-02 20:05:58 +0300

    second commit

--- resources/views/components/partials/footer.blade.php
+++ resources/views/components/partials/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="bg-white border-t border-gray-200 py-4 px-6 mt-auto">
     <div class="flex flex-col md:flex-row justify-between items-center">
         <div class="text-gray-600 text-sm">
-            &copy; 2024 Nish Auto Limited. All rights reserved.
+            &copy; 2026 Nish Auto Limited. All rights reserved.
         </div>
         <div class="flex space-x-4 mt-2 md:mt-0">
             <a href="/privacy" class="text-gray-600 hover:text-blue-600 text-sm">Privacy Policy</a>
